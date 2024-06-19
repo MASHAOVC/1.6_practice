378497d9962a067f9336b6f42d90c2e64a669c1a
Maria <masha-kem133@mail.ru>
2024-06-19 13:50:45 +0200

    gav

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
 import '../scss/style.scss'
 import './slider.js'
 import './show-more-brands.js'
+import './show-more-devices.js'
---
+++ src/js/show-more-devices.js
@@ -0,0 +1,64 @@
+// TODO remove duplication
+
+let showMoreButton = document.querySelector('.show-more-element__devices');
+let [icon, text] = showMoreButton.children;
+let hiddenItems768 = document.querySelectorAll('.devices-list__list-item--hidden768');
+let hiddenItems1100 = document.querySelectorAll('.devices-list__list-item--hidden1100');
+
+let isOpen = false;
+
+function normalize() {
+    if (window.innerWidth < 768) {
+        showAll();
+        return;
+    }
+
+    if (isOpen) {
+        showAll();
+    } else {
+        if (window.innerWidth >= 768 && window.innerWidth < 1100) {
+
+            for (let i = 0; i < hiddenItems768.length; i++) {
+                let item = hiddenItems768[i];
+                item.style.display = 'none';
+            }
+
+        } else if (window.innerWidth >= 1100) {
+
+            for (let i = 0; i < hiddenItems768.length; i++) {
+                let item = hiddenItems768[i];
+                item.style.display = 'flex';
+            }
+            for (let i = 0; i < hiddenItems1100.length; i++) {
+                let item = hiddenItems1100[i];
+                item.style.display = 'none';
+            }
+
+        }
+    }
+}
+
+function showAll() {
+    for (let i = 0; i < hiddenItems768.length; i++) {
+        let item = hiddenItems768[i];
+        item.style.display = 'flex';
+    }
+}
+
+showMoreButton.addEventListener('click', function() {
+    isOpen = !isOpen;
+
+    if (isOpen) {
+        icon.classList.add('show-more-element__button--close')
+        icon.classList.remove('show-more-element__button--expand')
+        text.textContent = 'Скрыть';
+    } else {
+        icon.classList.add('show-more-element__button--expand')
+        icon.classList.remove('show-more-element__button--close')
+        text.textContent = 'Показать все';
+    }
+
+    normalize();
+});
+
+window.addEventListener('resize', normalize);
