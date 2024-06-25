bad92eb606ac5c99ecde70b62c02436913ef1c61
Maria <masha-kem133@mail.ru>
2024-06-25 15:55:58 +0200

    gav

--- src/js/side-menu.js
+++ src/js/side-menu.js
@@ -1,6 +1,28 @@
-let burgerButton = document.querySelector('.header__button-burger');
-let closeButton = document.querySelector('.side-menu__header-button-close');
-let sideMenu = document.querySelector('.side-menu');
-let mainPage = document.querySelector('.page');
-let overlay = document.querySelector('.overlay');
+let burgerButton = document.querySelector('.header__button-burger')
+let closeButton = document.querySelector('.side-menu__header-button-close')
+let sideMenu = document.querySelector('.side-menu')
+let mainPage = document.querySelector('.page')
+let overlay = document.querySelector('.overlay')
 
+function open() {
+  sideMenu.style.display = 'flex'
+  overlay.style.display = 'flex'
+}
+
+function close() {
+  sideMenu.style.display = 'none'
+  overlay.style.display = 'none'
+}
+
+function normalize() {
+  if (window.innerWidth > 1426) {
+    sideMenu.style.display = 'flex'
+  } else {
+    close()
+  }
+}
+
+burgerButton.addEventListener('click', open)
+closeButton.addEventListener('click', close)
+overlay.addEventListener('click', close)
+window.addEventListener('resize', normalize)
