875c076fc399a54f059929756e50cd31a876ec9b
Maria <masha-kem133@mail.ru>
2024-06-25 18:06:33 +0200

    gav

---
+++ src/js/modal.js
@@ -0,0 +1,9 @@
+let callButtonHeader = document.querySelector('.header__button-call')
+let callButtonSide = document.querySelector('.side-menu__footer-button-call')
+let closeButtonCall = document.querySelector('.modal-call__header-button')
+let overlay = document.querySelector('.overlay')
+
+let feedbackButtonHeader = document.querySelector('.header__button-chat')
+let feedbackButtonSide = document.querySelector('.side-menu__footer-button-chat')
+let closeButtonFeedback = document.querySelector('..modal-feedback__header-button')
+
--- src/js/side-menu.js
+++ src/js/side-menu.js
@@ -1,7 +1,6 @@
 let burgerButton = document.querySelector('.header__button-burger')
 let closeButton = document.querySelector('.side-menu__header-button-close')
 let sideMenu = document.querySelector('.side-menu')
-let mainPage = document.querySelector('.page')
 let overlay = document.querySelector('.overlay')
 
 burgerButton.addEventListener('click', open)
