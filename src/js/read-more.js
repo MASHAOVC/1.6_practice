407ee7d5add4827936bdf61a29bae14cbcbf0b60
Maria <masha-kem133@mail.ru>
2024-06-28 21:28:18 +0200

    gav

--- src/js/read-more.js
+++ src/js/read-more.js
@@ -1,18 +1,21 @@
 let readMoreButton = document.querySelector('.aboutus__readmore')
-let [icon, text] = readMoreButton.children;
+let [icon, text] = readMoreButton.children
+let paragraph = document.querySelector('.aboutus__paragraphs-aboutus-wrapper')
 
-let isOpen = false;
+let isOpen = false
 
-readMoreButton.addEventListener('click', function() {
-  isOpen = !isOpen;
+readMoreButton.addEventListener('click', function () {
+  isOpen = !isOpen
 
   if (isOpen) {
-      icon.classList.add('show-more-element__button--close')
-      icon.classList.remove('show-more-element__button--expand')
-      text.textContent = 'Скрыть';
+    icon.classList.add('show-more-element__button--close')
+    icon.classList.remove('show-more-element__button--expand')
+    text.textContent = 'Скрыть'
+    paragraph.classList.add('aboutus__paragraphs-aboutus-wrapper--open')
   } else {
-      icon.classList.add('show-more-element__button--expand')
-      icon.classList.remove('show-more-element__button--close')
-      text.textContent = 'Читать далее';
+    icon.classList.add('show-more-element__button--expand')
+    icon.classList.remove('show-more-element__button--close')
+    text.textContent = 'Читать далее'
+    paragraph.classList.remove('aboutus__paragraphs-aboutus-wrapper--open')
   }
 })
